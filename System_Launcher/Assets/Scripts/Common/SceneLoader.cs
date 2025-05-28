4cdd89fda311d7ca94ae1a0a475876fa7f913bd5
leeworm <kalrocse@gmail.com>
2025-05-28 11:50:43 +0900

    첫째날

---
+++ System_Launcher/Assets/Scripts/Common/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum SceneType
+{
+    Title,
+    Lobby,
+    InGame
+}
+public class SceneLoader : SingletonBehaviour<SceneLoader>
+{
+    // 지정된 씬으로 전환하는 메서드
+    public void LoadScene(SceneType sceneType)
+    {
+        Logger.Log($"{sceneType} scene loading...");
+
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneType.ToString());
+    }
+    // 현재 씬을 다시 로드하는 메서드
+    public void ReloadScene()
+    {
+        Logger.Log($"{SceneManager.GetActiveScene().name} scene loading...");
+
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
